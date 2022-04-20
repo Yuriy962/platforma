74ab4d78405a1789e16f2f014286ae05c3232902
Yuriy962 <Yura962Bur@mail.ru>
2022-04-20 20:03:16 +0400

    feat: add 3 sections to project - desktop + mobile

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import * as flsFunctions from "./modules/functions.js";
+import * as bootstrap from "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import $ from "jquery";
+import Inputmask from "../../node_modules/inputmask/dist/jquery.inputmask.min.js";
+import * as slick from "../../node_modules/slick-carousel/slick/slick.min.js";
+
+// flsFunctions.isWebp();
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.$ = $;
+  
+});
